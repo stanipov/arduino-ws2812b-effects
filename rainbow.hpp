cf6d16658a17f59793b3b9727b5e8127f16d777a
Stanislav Filippov <stanislav.filippov@gmail.com>
2024-02-18 20:04:53 -0500

    Added fadeout for a led (instead of sharp turn-off)

--- rainbow.hpp
+++ rainbow.hpp
@@ -35,7 +35,7 @@ void rainbow_step(CRGB *arr, uint8_t start_hue, uint8_t dHue, uint16_t delay_ms)
 }
 
 
-void gradual_fill(CRGB *arr, uint8_t hue, uint16_t delay_ms){
+void rainbow_gradual_fill(CRGB *arr, uint8_t hue, uint16_t delay_ms){
   for (int i = 0; i < numLeds; i++){
     arr[i].setHue(hue);
     delay(delay_ms);
@@ -47,8 +47,52 @@ void gradual_fill(CRGB *arr, uint8_t hue, uint16_t delay_ms){
     FastLED.show();
     delay(delay_ms);
   }
-
 }
+
+
+void gradual_fill_fade(CRGB *arr, uint8_t hue, uint16_t delay_ms, uint8_t fadeAmt){
+  uint8_t fade_nums = 255 / fadeAmt+1;
+  
+  for (int i = 0; i < numLeds; i++){
+    arr[i].setHue(hue);
+    FastLED.show();
+    delay(delay_ms);
+  }
+
+
+  for (uint8_t i=0; i<numLeds; i++){
+      for (uint8_t c=0; c<=fade_nums; c++){
+        arr[i] = CHSV(hue, 255, max(0, 255-fadeAmt*c));
+        FastLED.show();
+        delay(delay_ms);
+        }
+      }
+}
+
+
+void gradual_fill_fade2(CRGB *arr, uint8_t hue, uint16_t delay_ms, uint8_t fadeAmt){
+  uint8_t fade_nums = 255 / fadeAmt+1;
+  uint8_t l ;// = leds[i].getLuma();
+
+
+  for (int i = 0; i < numLeds; i++){
+    arr[i].setHue(hue);
+    //arr = applyGamma_video(arr, gammaCorr);
+    FastLED.show();
+    delay(delay_ms);
+  }
+
+
+  for (uint8_t i=0; i<numLeds; i++){
+    l = arr[i].getLuma();
+    arr[i] = CHSV(hue, 255, max(0, l-fadeAmt));
+    
+    //arr[i].fadeToBlackBy(max(0, l-fadeAmt));
+    FastLED.show();
+    delay(delay_ms);
+    }
+}
+
 
 
 void __rainobw_step(CRGB *arr, uint8_t start_hue, uint16_t delay_ms){
